999feb643c0d6aabfd3e1807127773b15a3326e8
Hasibur Rahman <rahman15-14616@diu.edu.bd>
2022-09-20 02:47:22 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ s_n = 7
 g_c = 0
 g_l = 3
 while g_c < g_l:
-    g = int(input(
-        "If you guess the number then you will find your true love. So Let's Try: "))
+    g = int(input("Guess: "))
     g_c += 1
     if g == s_n:
-        print("I love you kashfea")
+        print("....")
+        break
     else:
-        print("I love you turin")
+        print("....")
