e423536df896d8591fefc310ae79c2d9dc13bd22
Hasibur Rahman <rahman15-14616@diu.edu.bd>
2022-09-20 01:20:59 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+s_n = 7
+g_c = 0
+g_l = 3
+while g_c < g_l:
+    g = int(input(
+        "If you guess the number then you will find your true love. So Let's Try: "))
+    g_c += 1
+    if g == s_n:
+        print("I love you kashfea")
+    else:
+        print("I love you turin")
